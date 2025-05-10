436b88c4d777b58a41c7f7a567c79c580468fc0b
aboelanwarr <o.b1277@yahoo.com>
2025-05-10 22:18:42 +0300

    Refactor Alert component usage in App to utilize children for content, improving readability and consistency across alerts.

--- src/App.tsx
+++ src/App.tsx
@@ -10,34 +10,57 @@ function App() {
         title={"Upgrade your plan"}
       >
         <p>
-        Then go to your Vite based project and run npm link --global vite (or the package manager that that you used to link {""} <a href="/">vite globally</a>). Now restart the development server to ride on the bleeding edge!
+          Then go to your Vite based project and run npm link --global vite (or
+          the package manager that that you used to link {""}{" "}
+          <a href="/">vite globally</a>). Now restart the development server to
+          ride on the bleeding edge!
         </p>
       </Alert>
-      
-      <Alert
-        type={"alert-info"}
-        icon={<Info size={20} />}
-        title="Note"
-        description="Vite aims to provide SSR Guide support for common web development patterns. Before searching for a Vite or compatible Rollup plugin, check out the Features Guide, check out the Backend Integration guide instead."
-      />
+
+      <Alert type={"alert-info"} icon={<Info size={20} />} title={"Note"}>
+        <p>
+          Vite aims to provide SSR Guide support for common web development
+          patterns. Before searching for a Vite or compatible Rollup plugin,
+          check out the Features Guide, check out the Backend Integration guide
+          instead."
+        </p>
+      </Alert>
+
       <Alert
         type={"alert-error"}
         icon={<Ban size={20} />}
-        title="Something went wroung"
-        description="If you are working with a normal static file server (except way more powerful!), Vite is also capable of handling dependencies that resolve to out-of-root file system locations, which makes it usable even in a monorepo-based setup."
-      />
+        title={"Something went wroung"}
+      >
+        <p>
+          If you are working with a normal static file server (except way more
+          powerful!), Vite is also capable of handling dependencies that resolve
+          to out-of-root file system locations, which makes it usable even in a
+          monorepo-based setup."
+        </p>
+      </Alert>
       <Alert
         type={"alert-success"}
         icon={<CheckCheck size={20} />}
-        title="Your order has been processed"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatum illo reiciendis facere vitae esse, ullam molestias ipsa nesciunt? Minima."
-      />
+        title={"Your order has been processed"}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          voluptatum illo reiciendis facere vitae esse, ullam molestias ipsa
+          nesciunt? Minima."
+        </p>
+      </Alert>
+
       <Alert
         type={"alert-tips"}
         icon={<AlertTriangle size={20} />}
-        title="Tips & Tricks"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatum illo reiciendis facere vitae esse, ullam molestias ipsa nesciunt? Minima."
-      />
+        title={"Tips & Tricks"}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          voluptatum illo reiciendis facere vitae esse, ullam molestias ipsa
+          nesciunt? Minima.
+        </p>
+      </Alert>
     </div>
   );
 }
